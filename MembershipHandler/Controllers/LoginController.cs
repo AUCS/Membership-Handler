dc99b5f377a3587e2631afd9be61b502352bda8b
Lucas Sargent <JJ11teen@hotmail.com>
2016-05-12 16:10:57 +0930

    facebook

---
+++ MembershipHandler/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MembershipHandler.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        public string Get(int id)
+        {
+            var accessToken = "your access token here";
+            var client = new FacebookClient(accessToken);
+            dynamic me = client.Get("me");
+            string aboutMe = me.about;
+
+            return aboutMe;
+        }
+    }
+}
